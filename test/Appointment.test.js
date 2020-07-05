e397d5daa7264757999434e19b1b44568bdf51eb
mbrad26 <mbrad26@gmail.com>
2020-07-05 15:53:17 +0100

    Renders another customer first name

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -5,12 +5,19 @@ import { Appointment } from '../src/Appointment';
 describe('Appointment', () => {
   it('renders the customer first name', () => {
     const customer = { firstName: 'Ashley' };
-    const component = <Appointment customer={customer} />;
     const container = document.createElement('div');
-    document.body.appendChild(container);
 
-    ReactDom.render(component, container);
+    ReactDom.render(<Appointment customer={customer} />, container);
 
-    expect(document.body.textContent).toMatch('Ashley');
+    expect(container.textContent).toMatch('Ashley');
+  });
+
+  it('renders another customer first name', () => {
+    const customer = { firstName: 'Jordan' };
+    const container = document.createElement('div');
+
+    ReactDom.render(<Appointment customer={customer} />, container);
+
+    expect(container.textContent).toMatch('Jordan');
   });
 });
