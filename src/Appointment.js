e9e780333813f8d2be4312859844fa5e985dd796
mbrad26 <mbrad26@gmail.com>
2020-07-05 17:25:36 +0100

    AppointmentsDayView renders multiple appointments in an ol element

--- src/Appointment.js
+++ src/Appointment.js
@@ -4,6 +4,12 @@ export const Appointment = ({ customer }) => (
   <div>{ customer.firstName }</div>
 );
 
-export const AppointmentsDayView = () => (
-  <div id='appointmentsDayView'></div>
+export const AppointmentsDayView = ({ appointments }) => (
+  <div id='appointmentsDayView'>
+    <ol>
+      { appointments.map(appointment => (
+        <div key={appointment.startsAt} />
+      )) }
+    </ol>
+  </div>
 );
