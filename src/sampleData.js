cd223c7d81c55ed5f47056e3c00d9171e495a87d
mbrad26 <mbrad26@gmail.com>
2020-07-07 11:37:45 +0100

    installed and configured webpack

--- src/sampleData.js
+++ src/sampleData.js
@@ -2,7 +2,7 @@ const today = new Date();
 
 const at = hours => today.setHours(hours, 0);
 
-export const sampleAppointments = {
+export const sampleAppointments = [
   { startsAt: at(9), customer: { firstName: 'Charlie' } },
   { startsAt: at(10), customer: { firstName: 'Frankie' } },
   { startsAt: at(11), customer: { firstName: 'Casey' } },
@@ -12,4 +12,4 @@ export const sampleAppointments = {
   { startsAt: at(15), customer: { firstName: 'Alex' } },
   { startsAt: at(16), customer: { firstName: 'Jules' } },
   { startsAt: at(17), customer: { firstName: 'Stevie' } }
-}
+];
