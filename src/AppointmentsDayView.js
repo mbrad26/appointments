2e57b2bde245f69ebcbc8b2b72b24bfdc40d29e8
mbrad26 <mbrad26@gmail.com>
2020-07-07 12:38:01 +0100

    renders customer telephone number

---
+++ src/AppointmentsDayView.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const appointmentTimeOfDay = startsAt => {
+  const [h, m] = new Date(startsAt).toTimeString().split(':');
+  return `${h}:${m}`;
+}
+
+export const Appointment = ({ customer }) => (
+  <div id='appointmentView'>
+    <table>
+      <tbody>
+        <tr>
+          <td>Customer</td>
+          <td>
+            {customer.firstName} {customer.lastName}
+          </td>
+        </tr>
+        <tr>
+          <td>Phone number</td>
+          <td>{customer.phoneNumber}</td>
+        </tr>
+        <tr>
+          <td>Stylist</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Service</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Notes</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
+
+export const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+
+  return (
+    <div id='appointmentsDayView'>
+      <ol>
+        { appointments.map((appointment, i) => (
+          <li key={appointment.startsAt}>
+            <button
+              type='button'
+              onClick={ () => setSelectedAppointment(i) }>
+              {appointmentTimeOfDay(appointment.startsAt)}
+            </button>
+          </li>
+        )) }
+      </ol>
+      { appointments.length === 0 ? (
+        <p>There are no appointments scheduled for today.</p>
+      ) : (
+        <Appointment {...appointments[selectedAppointment]} />
+      )}
+
+    </div>
+  );
+};
