87cc649bbfbc786acd31f67219821b6516de58de
mbrad26 <mbrad26@gmail.com>
2020-07-09 14:28:37 +0100

    CustomerForm component renders the existing value for the first name

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -30,4 +30,11 @@ describe('CustomerForm', () => {
 
     expectToBeInputFieldOfTypeText(field);
   });
+
+  it('includes the existing value for the first name', () => {
+    render(<CustomerForm firstName='Ashley' />);
+    const field = form('customer').elements.firstName;
+
+    expect(field.value).toEqual('Ashley');
+  });
 });
