9baf275b4d631aee9584123e0afd46bba78c46f3
mbrad26 <mbrad26@gmail.com>
2020-07-09 14:18:38 +0100

    CustomerForm component renders a form with a first name input field

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -17,4 +17,13 @@ describe('CustomerForm', () => {
 
     expect(form('customer')).not.toBeNull();
   });
+
+  it('renders the first name field as a text box', () => {
+    render(<CustomerForm />);
+    const field = form('customer').elements.firstName;
+
+    expect(field).not.toBeNull();
+    expect(field.tagName).toEqual('INPUT');
+    expect(field.type).toEqual('text');
+  });
 });
