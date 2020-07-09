d1b9144b47ae3cca485bab84c11203c17cd9e6df
mbrad26 <mbrad26@gmail.com>
2020-07-09 13:43:46 +0100

    CustomerForm component renders a form

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -1,3 +1,18 @@
 import React from 'react';
-import { createContainer} from './domManipulators';
-import { CustomerForm} from '../src/CustomerForm';
+import { createContainer } from './domManipulators';
+import { CustomerForm } from '../src/CustomerForm';
+
+describe('CustomerForm', () => {
+  let container;
+  let render;
+
+  beforeEach(() => {
+    ({ render, container } = createContainer());
+  });
+
+  it('renders a form', () => {
+    render(<CustomerForm />);
+
+    expect(container.querySelector('form[id="customer"]')).not.toBeNull();
+  });
+});
