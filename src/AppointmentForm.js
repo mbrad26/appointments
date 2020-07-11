9bae9bff2c0c658eeea4cb66253b3ca9f62193fc
mbrad26 <mbrad26@gmail.com>
2020-07-11 15:00:17 +0100

    renders an empty cell at the start of the header row (10 ms)       ✓ renders a week of available dates (21 ms)

--- src/AppointmentForm.js
+++ src/AppointmentForm.js
@@ -1,34 +1,101 @@
 import React, { useState } from 'react';
 
-const TimeSlotTable = () => <table id='time-slots' />
+const timeIncrements = (numTimes, startTime, increment) =>
+  Array(numTimes)
+    .fill([startTime])
+    .reduce((acc, _, i) =>
+      acc.concat([startTime + i * increment])
+    );
 
-export const AppointmentForm = ({ selectableServices, service, onSubmit }) => {
-  const [appointment, setAppointment] = useState({ service })
+const dailyTimeSlots = (salonOpensAt, salonClosesAt) => {
+  const totalSlots = (salonClosesAt - salonOpensAt) * 2;
+  const startTime = new Date().setHours(salonOpensAt, 0, 0, 0);
+  const increment = 30 * 60 * 1000;
+  return timeIncrements(totalSlots, startTime, increment);
+};
+
+const weeklyDateValues = startDate => {
+  const midnight = new Date(startDate).setHours(0, 0, 0, 0);
+  const increment = 24 * 60 * 60 * 1000;
+  return timeIncrements(7, midnight, increment);
+};
+
+const toShortDate = timestamp => {
+  const [day, , dayOfMonth] = new Date(timestamp)
+    .toDateString()
+    .split(' ');
+  return `${day} ${dayOfMonth}`;
+};
+
+const toTimeValue = timestamp =>
+  new Date(timestamp).toTimeString().substring(0, 5);
+
+const TimeSlotTable = ({ salonOpensAt, salonClosesAt, today }) => {
+  const dates = weeklyDateValues(today);
+  const timeSlots = dailyTimeSlots(salonOpensAt, salonClosesAt);
+  return (
+    <table id="time-slots">
+      <thead>
+        <tr>
+          <th />
+          {dates.map(d => (
+            <th key={d}>{toShortDate(d)}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {timeSlots.map(timeSlot => (
+          <tr key={timeSlot}>
+            <th>{toTimeValue(timeSlot)}</th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export const AppointmentForm = ({
+  selectableServices,
+  service,
+  onSubmit,
+  salonOpensAt,
+  salonClosesAt,
+  today
+}) => {
+  const [appointment, setAppointment] = useState({ service });
 
   const handleServiceChange = ({ target: { value } }) =>
-    setAppointment(appointment => ({...appointment, service: value}));
+    setAppointment(appointment => ({
+      ...appointment,
+      service: value
+    }));
 
   return (
-    <form id='appointment' onSubmit={() => onSubmit(appointment)}>
-      <label htmlFor='service'>Salon service</label>
+    <form id="appointment" onSubmit={() => onSubmit(appointment)}>
+      <label htmlFor="service">Salon service</label>
       <select
-        id='service'
-        name='service'
+        name="service"
+        id="service"
         value={service}
-        onChange={handleServiceChange}
-        >
+        onChange={handleServiceChange}>
         <option />
-          {selectableServices.map(s => (
-            <option key={s}>{s}</option>
-          ))}
+        {selectableServices.map(s => (
+          <option key={s}>{s}</option>
+        ))}
       </select>
-      <TimeSlotTable />
+      <TimeSlotTable
+        salonOpensAt={salonOpensAt}
+        salonClosesAt={salonClosesAt}
+        today={today}
+      />
     </form>
   );
 };
 
-// default props to be used when required props are not explicitly provided
 AppointmentForm.defaultProps = {
+  today: new Date(),
+  salonOpensAt: 9,
+  salonClosesAt: 19,
   selectableServices: [
     'Cut',
     'Blow-dry',
