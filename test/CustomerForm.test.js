713010537403882ebbd9361e6f43f9084475dff7
mbrad26 <mbrad26@gmail.com>
2020-07-13 13:54:30 +0100

    created custom spy

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -10,57 +10,59 @@ describe('CustomerForm', () => {
     ({ render, container } = createContainer());
   });
 
+  const spy = () => {
+    let receivedArguments;
+    return {
+      fn: (...args) => (receivedArguments = args),
+      receivedArguments: () => receivedArguments,
+      receivedArgument: n => receivedArguments[n]
+    }
+  };
+
   const form = id => container.querySelector(`form[id="${id}"]`);
   const field = name => form('customer').elements[name];
   const labelFor = formElement =>
     container.querySelector(`label[for="${formElement}"]`);
-
   const expectToBeInputFieldOfTypeText = formElement => {
     expect(formElement).not.toBeNull();
     expect(formElement.tagName).toEqual('INPUT');
     expect(formElement.type).toEqual('text');
   };
-
   const itRendersAsATextBox = fieldName =>
     it('renders as a text box', () => {
       render(<CustomerForm />);
       expectToBeInputFieldOfTypeText(field(fieldName));
     });
-
   const itIncludesTheExistingValue = fieldName =>
     it('includes the existing value', () => {
       render(<CustomerForm {...{ [fieldName]: 'value' }} />);
       expect(field(fieldName).value).toEqual('value');
     });
-
   const itRendersALabel = (fieldName, text) =>
     it('renders a label', () => {
       render(<CustomerForm />);
       expect(labelFor(fieldName)).not.toBeNull();
       expect(labelFor(fieldName).textContent).toEqual(text);
     });
-
   const itAssignsAnIdThatMatchesTheLabelId = fieldName =>
     it('assigns an id that matches the label id', () => {
       render(<CustomerForm />);
       expect(field(fieldName).id).toEqual(fieldName);
     });
-
   const itSubmitsExistingValue = (fieldName, value) =>
     it('saves existing value when submitted', async () => {
-      let submitArg;
+      const submitSpy = spy();
       render(
         <CustomerForm
           { ...{[fieldName]: 'value'} }
-          onSubmit={customer => submitArg = customer}
+          onSubmit={submitSpy.fn}
         />
       );
       ReactTestUtils.Simulate.submit(form('customer'));
 
-      expect(submitArg).toBeDefined();
-      expect(submitArg[fieldName]).toEqual('value');
+      expect(submitSpy.receivedArguments()).toBeDefined();
+      expect(submitSpy.receivedArgument(0)[fieldName]).toEqual('value');
     });
-
   const itSubmitsNewValue = (fieldName, value) =>
     it('saves new value when submitted', async () => {
       expect.hasAssertions();
