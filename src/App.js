75624359202dc0a1b0e75845257e99139b5259a1
mbrad26 <mbrad26@gmail.com>
2020-07-16 12:36:19 +0100

    it has a button bar as the first child

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { AppointmentsDayViewLoader } from './AppointmentsDayViewLoader';
 
 export const App = () => {
   <React.Fragment>
+    <div className='button-bar' />
     <AppointmentsDayViewLoader />
   </React.Fragment>
-}
+};
--- test/App.test.js
+++ test/App.test.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { child, createShallowRenderer, type } from './shallowHelpers';
+import {
+  child,
+  createShallowRenderer,
+  type
+} from './shallowHelpers';
 import { App } from '../src/App';
 import { AppointmentsDayViewLoader } from '../src/AppointmentsDayViewLoader';
 
@@ -14,5 +18,12 @@ describe('App', () => {
     render(<App />);
 
     expect(elementMatching(type(AppointmentsDayViewLoader))).toBeDefined();
+  });
+
+  it('it has a button bar as the first child', () => {
+    render(<App />);
+
+    expect(child(0).type).toEqual('div');
+    expect(child(0).props.className).toEqual('button-bar');
   });
 });
